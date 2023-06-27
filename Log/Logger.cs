a3c13e2eb555a9660ef12a40f45a0306c3cbb2f3
Artem Nebogatikov <a.nebogatikov@yandex.ru>
2023-06-27 21:28:19 +0400

    Logger

---
+++ Log/Logger.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Log
+{
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public void Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
+
