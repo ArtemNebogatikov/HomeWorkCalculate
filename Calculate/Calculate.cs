4d9ae6ebf08298079304603ecf6bc2954c57fe86
Artem Nebogatikov <a.nebogatikov@yandex.ru>
2023-06-27 21:01:18 +0400

    Add project files.

---
+++ Calculate/Calculate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Log;
+
+namespace Calculate
+{
+    
+    public interface ISum
+    {
+        public int Sum(int a, int b);
+    }
+    class Calculate : ISum
+    {
+        ILogger Logger { get; }
+        public Calculate(ILogger logger)
+        {
+            Logger = logger;
+        }
+        public int Sum(int a, int b)
+        {
+            Logger.Error("Калькулятор начал свою работу");
+            Thread.Sleep(1000);
+            Logger.Event("Калькулятор закончил свою работу");
+            return a + b;
+        }
+        public int GetNum(int number)
+        {
+            int num = 0;
+            while(true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите {0} число", number);
+                    num = Convert.ToInt32(Console.ReadLine());
+                    return num;
+                }
+                catch
+                {
+                    Logger.Error("Введено некорректное значение");
+                }
+                
+            }
+            
+        }
+    }
+}
---
+++ Calculate/Program.cs
@@ -0,0 +1,27 @@
+﻿using Log;
+
+namespace Calculate
+{
+
+    class Program
+    {
+        static ILogger Logger { get; set; }
+        static void Main(string[] args)
+        {
+            Logger = new Logger();
+            Console.WriteLine("Мини калькулятор суммы");
+            Calculate calc = new Calculate(Logger);
+            int num1 = calc.GetNum(1);
+            int num2 = calc.GetNum(2);
+            Console.WriteLine("Сумма равна: {0}", calc.Sum(num1, num2));
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
+
+
+
